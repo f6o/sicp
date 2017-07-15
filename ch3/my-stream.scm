3ed575ddb876c59db74123f652bbac6dc0c718cb
f6o <fluffy6h@gmail.com>
2017-07-15 15:49:09 +0900

    add stream-filter

--- ch3/my-stream.scm
+++ ch3/my-stream.scm
@@ -10,6 +10,9 @@
 (define the-null-stream
   '())
 
+(define (stream-null? s)
+  (null? s))
+
 (define (stream-enumerate-integer a b)
   (if (> a b)
       the-null-stream
@@ -21,3 +24,12 @@
       (stream-car s)
       (stream-ref (stream-cdr s) (- n 1))))
 
+(define (stream-filter pred? s)
+  (cond ((stream-null? s)
+	 the-null-stream)
+	((pred? (stream-car s))
+	 (cons-stream (stream-car s)
+		      (stream-filter pred? (stream-cdr s))))
+	(else (stream-filter pred? (stream-cdr s)))))
+
+      
