0af5c69422f39fe9eb6097f2fbce583bc89976e2
f6o <rebhoon@gmail.com>
2017-07-19 20:30:08 +0900

    update exercise 2.54

--- ch2/excercises.scm
+++ ch2/excercises.scm
@@ -25,10 +25,10 @@
 
 (define (equal? a b)
   (if (and (pair? a) (pair? b))
-      (if (eq? (car a) (car b))
+      (if (equal? (car a) (car b))
           (equal? (cdr a) (cdr b))
-            #f)
-      (and (null? a) (null? b))))
+          #f)
+      (eq? a b)))
 
 (equal? '(this is a list) '(this is (a test) list))
 
