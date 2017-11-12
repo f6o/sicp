90234e34ebe5157154dbf70ec768f1a0c5fb1887
f6o <fluffy6h@gmail.com>
2017-11-13 08:25:49 +0900

    add comment for running test

--- ch4/driver-loop.test.scm
+++ ch4/driver-loop.test.scm
@@ -1,8 +1,10 @@
+;; run gosh -l./my-evaluator-3.scm driver-loop.test.scm
 (define the-global-environment (setup-environment))
 (eval '(define (f x) (+ x 1)) the-global-environment)
 (define result (eval '(f 10) the-global-environment))
 (if (not (= result 10))
     (error "NG"))
+
 
 
 
