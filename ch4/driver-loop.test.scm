fbfe182a8009f0c0074e496cfac8cc1469383895
f6o <fluffy6h@gmail.com>
2017-11-16 08:05:52 +0900

    add test cases

--- ch4/driver-loop.test.scm
+++ ch4/driver-loop.test.scm
@@ -6,6 +6,13 @@
 	(begin (display "OK") (newline))
 	(error #"FAILS: ~|message|"))))
 (eval '(define (f x) (+ x 1)) the-global-environment)
+(eval '(define a (f 1)) the-global-environment)
+(eval '(define b (+ a 1)) the-global-environment)
 
-(eval-assert = 11 '(f 10) "NG")
+(eval-assert = 11 '(f 10) "function call")
+(eval-assert = 2 'a "defining a global value")
+(eval-assert = 3 'b "defining a global value using another var")
+
+(eval '(define a 10) the-global-environment)
+(eval-assert = 10 'a "redefining a global value")
 
