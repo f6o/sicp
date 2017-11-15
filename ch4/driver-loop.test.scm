46b2df7462fb580962a3f3e1c6b86b4b2ef02632
f6o <fluffy6h@gmail.com>
2017-11-15 21:53:53 +0900

    add test case

--- ch4/driver-loop.test.scm
+++ ch4/driver-loop.test.scm
@@ -2,10 +2,5 @@
 (define the-global-environment (setup-environment))
 (eval '(define (f x) (+ x 1)) the-global-environment)
 (define result (eval '(f 10) the-global-environment))
-(if (not (= result 10))
+(if (not (= result 11))
     (error "NG"))
-
-
-
-
-
