7e6e00a45b7631dea1369f59aef4b97fa29122c0
f6o <fluffy6h@gmail.com>
2017-11-16 07:21:43 +0900

    add utility function

--- ch4/driver-loop.test.scm
+++ ch4/driver-loop.test.scm
@@ -1,6 +1,11 @@
 ;; run gosh -l./my-evaluator-3.scm driver-loop.test.scm
 (define the-global-environment (setup-environment))
+(define (eval-assert test expected expr message)
+  (let ((actual (eval expr the-global-environment)))
+    (if (test actual expected)
+	(begin (display "OK") (newline))
+	(error #"FAILS: ~|message|"))))
 (eval '(define (f x) (+ x 1)) the-global-environment)
-(define result (eval '(f 10) the-global-environment))
-(if (not (= result 11))
-    (error "NG"))
+
+(eval-assert = 11 '(f 10) "NG")
+
