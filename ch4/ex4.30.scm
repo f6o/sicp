02f899e212949447e9f02cccf5bb9d89c5fe726a
f6o <fluffy6h@gmail.com>
2020-01-16 15:21:33 +0900

    for better debugging

--- ch4/ex4.30.scm
+++ ch4/ex4.30.scm
@@ -8,5 +8,15 @@
             (actual-value (first-exp exps) env)
             (eval-sequence (rest-exps exps) env))))
 
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (if (eq? input 'quit)
+        (begin (newline) (display "exit drive-loop") (newline))
+        (let ((output (eval input the-global-environment)))
+          (announce-output output-prompt)
+        (user-print output)
+        (driver-loop)))))
+
 (define the-global-environment (setup-environment))
 (driver-loop)
--- ch4/run-ex4.30.sh
+++ ch4/run-ex4.30.sh
@@ -7,12 +7,6 @@ function run {
     echo -n "$EXP" | gosh $SCRIPT --
 }
 
-echo "(p1 1)"
-run "(define (p1 x) (set! x (cons x '(2))) x) (p1 1)"
-
-echo "(p2 1)"
-run "(define (p2 x) (define (p e) e x) (p (set! x (cons x '(2))))) (p2 1)"
-
-
-
+run "(define (p1 x) (set! x (cons x '(2))) x) (p1 1) quit"
+run "(define (p2 x) (define (p e) e x) (p (set! x (cons x '(2))))) (p2 1) quit"
 
