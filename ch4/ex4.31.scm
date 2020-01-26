59a6ba1c45074efd8921849adf63ff552fd14803
f6o <fluffy6h@gmail.com>
2020-01-26 20:43:06 +0900

    add some notes

--- ch4/ex4.31.scm
+++ ch4/ex4.31.scm
@@ -5,10 +5,14 @@
 ;; (define (f a (b lazy) c (d lazy-memo))
 ;;     ...)
 
-;;
-;; eval calls lambda-parameters if the expression is lambda
-;; apply calls procedure-parameters if the procedure is compound (i.e. not primitive)
-;;
+#|
+  
+* eval calls lambda-parameters if the expression is lambda
+* apply calls procedure-parameters if the procedure is compound (i.e. not primitive)
+* each argument with lazy or lazy-memo is to be applied with list-of-delayed-args
+* Q: What is the implementation difference between lazy and lazy-memo
+ 
+|#
 
 (define the-global-environment (setup-environment))
 (driver-loop)
